a60e7f8573d7e26fd9869caf012429cba730a071
Dmytro Sokotniuk <dimastasokotnyuk@gmail.com>
2025-07-15 20:28:48 +0300

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Дмитро Сокотнюк on 15.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
